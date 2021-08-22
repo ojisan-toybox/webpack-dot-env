5418dcfd1a38c2b126748b09f700340f031e298a
sadnessOjisan <sadnessOjisan@users.noreply.github.com>
2021-08-22 13:01:33 +0000

    deploy: ecbcec66083a15ce026f28ab028978967d244f68

--- build.js
+++ build.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const o=process.env.HOGE,e=process.env.FUGA;if(void 0===o||void 0===e)throw new Error("環境変数がたりません。");const n=o,r=e,t=document.getElementById("root");if(null===t)throw new Error("not found el");console.log(t),t.innerText=`hoge: ${n}, fuga: ${r}`})();
+(()=>{"use strict";const e=document.getElementById("root");if(null===e)throw new Error("not found el");console.log(e),e.innerText="hoge: hogeeee, fuga: fugaaa"})();
