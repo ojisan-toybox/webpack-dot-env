4a0823bcfcc5684b5ff77f2a2dce9b6f604faa49
sadness_ojisan <sadness.ojisan@gmail.com>
2021-08-22 21:40:50 +0900

    Add deploy yaml

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const dotenv = require("dotenv");
+
+const env = dotenv.config().parsed;
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,5 +10,15 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "build.js",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin(),
+    env !== undefined
+      ? new webpack.DefinePlugin({
+          "process.env": JSON.stringify(env),
+        })
+      : new webpack.DefinePlugin({
+          "process.env.HOGEHOGE": JSON.stringify(process.env.HOGE),
+          "process.env.FUGAFUGA": JSON.stringify(process.env.FUGA),
+        }),
+  ],
 };
